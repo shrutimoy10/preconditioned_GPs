2f96d4f2a9fc93e1cfc5c2f208242996d728aa0e
Shrutimoy Das <shrutimoy@gmail.com>
2018-12-10 22:00:23 +0530

    Add files via upload

--- code/pcgComparison/PcgComp/PcgComp/util/__init__.py
+++ code/pcgComparison/PcgComp/PcgComp/util/__init__.py
@@ -1,3 +1,3 @@
-from kronHelper import KronHelper
-from inducingPointsHelper import InducingPointsHelper
-from ssgp import SsgpHelper
+from PcgComp.util.kronHelper import KronHelper
+from PcgComp.util.inducingPointsHelper import InducingPointsHelper
+from PcgComp.util.ssgp import SsgpHelper
